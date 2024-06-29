fea8faa0985f7f5624c604825c65ab9c23240b4f
3pdoul <abdulrahman.h.mustafa@gmail.com>
2024-06-29 14:55:46 -0700

    7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const array = process.argv;
+if (array.length > 2) {
+  for (let i = 0; i < array[2]; i++) {
+    console.log('C is fun');
+  }
+}
+else {
+  console.log('Missing number of occurrences');
+}
