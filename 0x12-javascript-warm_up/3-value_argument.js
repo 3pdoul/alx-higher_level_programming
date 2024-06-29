843d23ce7d35d34a503d1da810029e0173989974
3pdoul <abdulrahman.h.mustafa@gmail.com>
2024-06-29 03:58:33 -0700

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const arg = (process.argv);
-if (arg.length === 2) {
-	console.log("No argument");
-}
-else {
-	console.log(arg[2]);
+if (!(2 in arg)) {
+  console.log('No argument');
+} else {
+  console.log(arg[2]);
 }
