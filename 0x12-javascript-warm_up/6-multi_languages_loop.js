9b89844c930f70786a7f3338fa05d05f90a3dec0
3pdoul <abdulrahman.h.mustafa@gmail.com>
2024-06-29 14:48:31 -0700

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const array = [];
+array.push('C is fun');
+array.push('Python is cool');
+array.push('JavaScript is amazing');
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
