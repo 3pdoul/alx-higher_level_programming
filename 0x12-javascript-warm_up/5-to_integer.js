28d0a7bc7da736fc918b35b2c8136e3162bc418e
3pdoul <abdulrahman.h.mustafa@gmail.com>
2024-06-29 14:42:08 -0700

    semistandard

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const arg = (process.argv);
-const num = parseInt(arg[2], 10) 
-if (isNaN(num)){
-	console.log('Not a number'); 
-}
-else{
-	console.log('My number: ${num}');
+const num = parseInt(arg[2], 10);
+if (isNaN(num)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + num);
 }
